d908a857f2c1a2a61ffa73374ef3e426459335ea
Jieun <jeeneep@naver.com>
2025-10-26 19:18:21 +0900

    feat(app): Add application entry point and main flow

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,40 @@
 package racingcar;
 
+import racingcar.domain.Car;
+import racingcar.domain.RacingGame;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        RacingGame game = initializeGame(inputView);
+        runGame(game, outputView);
+        printWinners(game, outputView);
+    }
+
+    private static RacingGame initializeGame(InputView inputView) {
+        String carNames = inputView.inputCarNames();
+        String rounds = inputView.inputRounds();
+
+        return new RacingGame(carNames, rounds);
+    }
+
+    private static void runGame(RacingGame game, OutputView outputView) {
+        outputView.printResultHeader();
+
+        for (int i = 0; i < game.getTotalRounds(); i++) {
+            List<Car> roundResult = game.proceedOneRound();
+            outputView.printRoundResult(roundResult);
+        }
+    }
+
+    private static void printWinners(RacingGame game, OutputView outputView) {
+        List<String> winners = game.getWinners();
+        outputView.printWinners(winners);
     }
 }
